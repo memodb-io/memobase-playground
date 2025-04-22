f3cc4822a3cea65f8f49c691019c29dc9c4e1d63
Gener <435669237@qq.com>
2025-04-22 23:27:40 +0800

    Revert "fix: Change the default theme from the system to a bright color theme to improve the user interface experience"
    This reverts commit 59130948318799f002fe5e826e7328d2c0ea33d2.

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export default async function RootLayout({
         <NextIntlClientProvider messages={messages}>
           <ThemeProvider
             attribute="class"
-            defaultTheme="light"
+            defaultTheme="system"
             enableSystem
             disableTransitionOnChange
           >
