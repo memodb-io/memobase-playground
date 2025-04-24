8e7925a3409868052f51bf96f371ebe07a429753
Gener <435669237@qq.com>
2025-04-24 16:02:37 +0800

    feat: Add the basePath setting to "/playground" in the Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,7 @@ const withNextIntl = createNextIntlPlugin();
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
+  basePath: "/playground",
 };
 
 export default withNextIntl(nextConfig);
