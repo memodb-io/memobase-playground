9a6c25dcbb134e621eb02fb4aca253781704e5e6
Gener <435669237@qq.com>
2025-05-02 18:03:09 +0800

    fix: del console

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -41,8 +41,6 @@ export async function POST(req: Request) {
         ])
       ),
     });
-    console.log(finalSystemPrompt);
-    console.log(result);
 
     const lastMessage =
       messages[messages.length - 1].content[
