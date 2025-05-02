d5b0c67c96d5f4c42da4cfeb8373f83916b60ecb
Gener <435669237@qq.com>
2025-05-02 18:59:19 +0800

    feat: Add email redirection option in registration function to optimize user experience

--- app/signup/actions.ts
+++ app/signup/actions.ts
@@ -11,6 +11,11 @@ export async function signup(formData: FormData) {
   const { error, data } = await supabase.auth.signUp({
     email: formData.get("email") as string,
     password: formData.get("password") as string,
+    options: {
+      emailRedirectTo:
+        process.env.NEXT_PUBLIC_BASE_URL! + process.env.NEXT_PUBLIC_BASE_PATH ||
+        "",
+    },
   });
 
   if (error) {
