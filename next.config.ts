fe630c065f61c1d57caeb90bdd95e5f5cc4fa839
Gener <435669237@qq.com>
2025-06-27 16:58:55 +0800

    fix: next config

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,14 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.md$/,
+      resourceQuery: /raw/,
+      type: 'asset/source',
+    });
+    return config;
+  },
 };
 
 export default withNextIntl(nextConfig);
